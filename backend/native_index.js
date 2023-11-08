877e5328122f3cd4c56e4dcf5d7cc3382c96940b
Naveed <malikdesigner6453@gnmail.com>
2023-11-08 20:42:44 +0500

    check

--- backend/native_index.js
+++ backend/native_index.js
@@ -20,8 +20,12 @@ const db = mysql.createConnection({
 //     methods: ["POST", "GET"],
 //     credentials: true
 // }))
-app.use(cors());
-db.connect((err) => {
+const corsOptions = {
+    origin: 'http://192.168.100.122:3301',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions)); db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err);
         process.exit(1); // Exit the process on connection error
@@ -45,53 +49,20 @@ app.post('/addBooks', async (req, res) => {
         const sql = `INSERT INTO tbl_books (name, author, date_added, email, added_by) 
             VALUES (?, ?, CURRENT_TIMESTAMP, ?, ?)`;
 
-
         const result = await query(sql, [bookName, bookAuthor, email, addedBy]);
 
         console.log('Record inserted successfully');
-        res.status(200).json({ ok: true, message: 'Record inserted successfully' });
+        res.status(200).json({ success: true, message: 'Record inserted successfully' });
     } catch (error) {
         console.error('Error inserting record:', error);
-        res.status(500).json({ ok: false, message: 'Internal Server Error' });
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 });
 app.get('/books', (req, res) => {
-    const { search } = req.query;
 
-    if (search) {
-        // If there's a search query, perform a search
-        const sql = 'SELECT * FROM tbl_books WHERE name LIKE ? OR author LIKE ?';
-        const searchQuery = `%${search}%`;
+    const sql = 'SELECT * FROM tbl_books';
 
-        db.query(sql, [searchQuery, searchQuery], (err, result) => {
-            if (err) {
-                console.error('Error executing search query:', err);
-                res.status(500).json({ error: 'Internal Server Error' });
-            } else {
-                console.log(result);
-                res.status(200).json(result);
-            }
-        });
-    } else {
-        // If no search query, fetch all books
-        const sql = 'SELECT * FROM tbl_books';
-
-        db.query(sql, (err, result) => {
-            if (err) {
-                console.error('Error executing query:', err);
-                res.status(500).json({ error: 'Internal Server Error' });
-            } else {
-                console.log(result);
-                res.status(200).json(result);
-            }
-        });
-    }
-});
-app.get('/books/:id', (req, res) => {
-    const { id } = req.params;
-    const sql = 'SELECT * FROM tbl_books WHERE id = ?';
-
-    db.query(sql, [id], (err, result) => {
+    db.query(sql, (err, result) => {
         if (err) {
             console.error('Error executing query:', err);
             res.status(500).json({ error: 'Internal Server Error' });
@@ -100,13 +71,29 @@ app.get('/books/:id', (req, res) => {
             res.status(200).json(result);
         }
     });
+
+});
+app.get('/books/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = 'SELECT * FROM tbl_books WHERE id = ?';
+    console.log("REQESUT BY ID")
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            //console.log(result[0]);
+            res.status(200).json(result[0]);
+        }
+    });
 });
 app.put('/books/:id', async (req, res) => {
+    console.log('Received PUT request for /books/:id');
     try {
         const { id } = req.params;
-        const { bookName, bookAuthor, email, addedBy } = req.body;
+        const { name, author, email, added_by } = req.body;
         console.log(req.body);
-        console.log(req);
+        console.log("REQ INSERION")
         const sql = `
         UPDATE tbl_books
         SET
@@ -117,7 +104,9 @@ app.put('/books/:id', async (req, res) => {
         WHERE id = ?
       `;
 
-        const result = await query(sql, [bookName, bookAuthor, email, addedBy, id]);
+        // const result = await query(sql, [bookName, bookAuthor, email, addedBy, id]);
+        const convertedId = parseInt(id, 10);
+        const result = await query(sql, [name, author, email, added_by, convertedId]);
 
         if (result.affectedRows > 0) {
             console.log(`Book with id ${id} updated successfully`);
